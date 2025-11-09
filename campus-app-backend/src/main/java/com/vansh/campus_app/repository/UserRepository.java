6886c0d496eeac1e67a06e227bc2e88bcfaa6363
Vanshdeep-04 <vansh.chd412@gmail.com>
2025-11-09 01:34:15 -0800

    Removed old backend prototype

--- campus-app-backend/src/main/java/com/vansh/campus_app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vansh.campus_app.repository;
-
-import com.vansh.campus_app.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-}
